f5321e135efc0999a14255529de4e39a25fd6278
Gautam855 <131901652+Gautam855@users.noreply.github.com>
2024-08-19 18:53:02 +0530

    Add files via upload

---
+++ Calculator/cal.js
@@ -0,0 +1,52 @@
+document.addEventListener('keydown', function (event) {
+    var key = event.key;
+
+    switch (key) {
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+        case '.':
+            Solve(key);
+            break;
+        case 'Enter':
+            Result();
+            break;
+        case 'Escape':
+            Clear();
+            break;
+        case 'Backspace':
+            Back();
+            break;
+        default:
+            break;
+    }
+});
+
+function Solve(val) {
+    var v = document.getElementById('res');
+    v.value += val;
+ }
+ function Result() {
+    var num1 = document.getElementById('res').value;
+    var num2 = eval(num1);
+    document.getElementById('res').value = num2;
+ }
+ function Clear() {
+    var inp = document.getElementById('res');
+    inp.value = '';
+ }
+ function Back() {
+    var ev = document.getElementById('res');
+    ev.value = ev.value.slice(0,-1);
+ }
